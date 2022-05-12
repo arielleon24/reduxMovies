640581f804fd6547d8bedcdcf6d60e15670817c7
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2022-05-12 14:32:57 -0400

    routing function (except pageNotFound)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import './App.scss';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import MovieDetail from './components/MovieDetail/MovieDetail';
+import PageNotFound from './components/PageNotFound/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className="app">
+     <Router>
+       <Header></Header>
+       <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route path="/movie/:imdbID" element={<MovieDetail />} />
+        <Route element={<PageNotFound />}/>
+       </Routes>
+       <Footer />
+     </Router>
     </div>
   );
 }
